384e9409aff7341f8810160e98dc3253547e3dd2
AislanJTelesF <aislanjesus2019@gmail.com>
2025-07-22 11:50:07 -0300

    consertando a página de comparar carros

--- src/app/pages/comparacao/comparacao.spec.ts
+++ src/app/pages/comparacao/comparacao.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Comparacao } from './comparacao';
+import { ComparacaoComponent } from './comparacao';
 
 describe('Comparacao', () => {
-  let component: Comparacao;
-  let fixture: ComponentFixture<Comparacao>;
+  let component: ComparacaoComponent;
+  let fixture: ComponentFixture<ComparacaoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Comparacao]
+      imports: [ComparacaoComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Comparacao);
+    fixture = TestBed.createComponent(ComparacaoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
