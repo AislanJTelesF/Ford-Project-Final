7467d5367251649096b5d6e1e1c672094228cb36
AislanJTelesF <aislanjesus2019@gmail.com>
2025-07-25 11:08:38 -0300

    .

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+    // src/environments/environment.ts
+    export const environment = {
+      production: false,
+      apiUrl: 'http://localhost:3001/api' // URL da API para desenvolvimento local
+    };
+    
