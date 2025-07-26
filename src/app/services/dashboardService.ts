dc64240a576f84c871fc3ca4d1beeadd69e710d3
AislanJTelesF <aislanjesus2019@gmail.com>
2025-07-26 13:16:15 -0300

    consertando o src/app/services/dashboardService.ts para o 'vin' e o 'selecionar carros' funcionar

--- src/app/services/dashboardService.ts
+++ src/app/services/dashboardService.ts
@@ -1,28 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+// src/app/services/dashboardService.ts
 import { Injectable } from '@angular/core';
-import { VeiculosAPI } from '../models/veiculo.model';
-import { CarroVin } from '../utils/carroVinInterface';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { Veiculo } from '../models/veiculo.model'; // Certifique-se que o caminho está correto
+import { CarroVin } from '../utils/carroVinInterface'; // Certifique-se que o caminho está correto
+import { environment } from '../../environments/environment'; // Importa o ambiente
 
 @Injectable({
   providedIn: 'root',
 })
 export class DashboardService {
-  private apiUrl = environment.apiUrl;
+  private apiUrl = environment.apiUrl; // Usa a URL da API do arquivo de ambiente
 
   constructor(private http: HttpClient) {}
 
-
-
-  getVehicles(): Observable<VeiculosAPI> {
-    return this.http.get<VeiculosAPI>(`${this.apiUrl}/api/vehicles`);
+  getVehicles(): Observable<{ vehicles: Veiculo[] }> {
+    return this.http.get<{ vehicles: Veiculo[] }>(`${this.apiUrl}/vehicles`);
   }
 
-  buscarVin(codigoVin: string): Observable<CarroVin> {
-    const reqVin = this.http.post<CarroVin>(`${this.apiUrl}/api/vehicleData`, {
-      vin: codigoVin,
-    });
-    return reqVin;
+  buscarVin(vin: string): Observable<CarroVin> {
+    return this.http.post<CarroVin>(`${this.apiUrl}/vehicleData`, { vin });
   }
 }
